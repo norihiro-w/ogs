c77964c10275c7cc22026e3dbe57acd576203861
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2015-08-06 15:27:32 +0200

    [Ex] use same Lis setting

--- MyModules/Examples/FemGroundwaterFlow/tests/liquid_flow_2d/compare.sh
+++ MyModules/Examples/FemGroundwaterFlow/tests/liquid_flow_2d/compare.sh
@@ -1,5 +1,8 @@
 echo "# ogs6-thmc"
-time /home/localadmin/ogs/ogs6/ogs6-thmc/BuildClangRelease/bin/ogs6 q_quad &> ogs.log
+time /home/localadmin/ogs/ogs6/ogs6-thmc/BuildClangRelease/bin/ogs6 q_quad -i 2 -p 0 -initx_zeros 1 &> ogs.log
+#echo ""
+#echo "# ogs6-eigen"
+#time /home/localadmin/ogs/ogs6/ogs6-thmc/BuildClangRelease/bin/ogs6-eigen q_quad -i 2 -p 0 -initx_zeros 1 &> ogs.log
 echo ""
 echo "#ogs5-mkl"
 time /home/localadmin/ogs/ogs5/trunk/trunk-sources-git-ufz/BuildMKLRelease/bin/ogs q_quad &> ogs5.log
