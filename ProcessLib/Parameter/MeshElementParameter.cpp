4406b500d11b034faa7f91e290fd53d0a651ad19
Norihiro Watanabe <norihiro.watanabe@aist.go.jp>
2019-01-24 22:10:05 +0900

    [PL] add create, components tags to MeshNode/ElementParameter

--- ProcessLib/Parameter/MeshElementParameter.cpp
+++ ProcessLib/Parameter/MeshElementParameter.cpp
@@ -20,19 +20,40 @@ std::unique_ptr<ParameterBase> createMeshElementParameter(
     //! \ogs_file_param{prj__parameters__parameter__type}
     config.checkConfigParameter("type", "MeshElement");
     //! \ogs_file_param{prj__parameters__parameter__MeshElement__field_name}
-    auto const field_name = config.getConfigParameter<std::string>("field_name");
+    auto const field_name =
+        config.getConfigParameter<std::string>("field_name");
     DBUG("Using field_name %s", field_name.c_str());
+
+    auto const hasProperty =
+        mesh.getProperties().existsPropertyVector<double>(field_name);
+    auto const create =
+        //! \ogs_file_param{prj__parameters__parameter__MeshElement__create}
+        config.getConfigParameterOptional<std::string>("create");
+    auto opt_n_comp =
+        //! \ogs_file_param{prj__parameters__parameter__MeshElement__components}
+        config.getConfigParameterOptional<unsigned>("components");
+    if (!hasProperty && create && create.get() == "true")
+    {
+        DBUG("Property %s does not exit. create", field_name.c_str());
+        unsigned const n_comp = opt_n_comp ? opt_n_comp.get() : 1;
+
+        auto new_property = const_cast<MeshLib::Mesh&>(mesh)
+                                .getProperties()
+                                .createNewPropertyVector<double>(
+                                    field_name, MeshLib::MeshItemType::Cell, n_comp);
+        new_property->resize(mesh.getNumberOfElements() * n_comp);
+    }
 
     // TODO other data types than only double
     auto const& property =
         mesh.getProperties().getPropertyVector<double>(field_name);
 
-    if (property->getMeshItemType() != MeshLib::MeshItemType::Cell) {
+    if (property->getMeshItemType() != MeshLib::MeshItemType::Cell)
+    {
         OGS_FATAL("The mesh property `%s' is not an element property.",
                   field_name.c_str());
     }
-
     return std::make_unique<MeshElementParameter<double>>(name, *property);
 }
 
-}  // ProcessLib
+}  // namespace ProcessLib
