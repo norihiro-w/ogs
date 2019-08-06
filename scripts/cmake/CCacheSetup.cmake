9a5d1f8d7a3dbaa197f95d6d3a2e3d17b91f8bc8
Norihiro Watanabe <norihiro.watanabe@aist.go.jp>
2019-08-07 06:36:01 +0900

    [cmake] no check of pch_defines if ccache < 3.2.5

--- scripts/cmake/CCacheSetup.cmake
+++ scripts/cmake/CCacheSetup.cmake
@@ -7,11 +7,15 @@ set(CCACHE_VERSION_REQUIRED 3.2.0)
 execute_process(COMMAND ${CCACHE_TOOL_PATH} --version
     OUTPUT_VARIABLE CCACHE_VERSION
 )
+set(CCACHE_VERSION_GREATER_EQUAL_3_2_5 ON)
 if("${CCACHE_VERSION}" MATCHES "ccache version ([0-9]\\.[0-9]\\.[0-9])")
     if(${CMAKE_MATCH_1} VERSION_LESS ${CCACHE_VERSION_REQUIRED})
         message(STATUS "CCache outdated. Installed: ${CMAKE_MATCH_1}, \
             required: ${CCACHE_VERSION_REQUIRED}. Caching disabled.")
         return()
+    endif()
+    if(${CMAKE_MATCH_1} VERSION_LESS 3.2.5)
+        set(CCACHE_VERSION_GREATER_EQUAL_3_2_5 OFF)
     endif()
 endif()
 
@@ -28,18 +32,27 @@ if($ENV{TRAVIS})
 endif()
 
 # Check ccache pre-compiled headers config
-execute_process(COMMAND ${CCACHE_TOOL_PATH} --print-config
+execute_process(COMMAND ${CCACHE_TOOL_PATH} -p
     OUTPUT_VARIABLE CCACHE_CONFIG
     ERROR_VARIABLE CCACHE_CONFIG
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE
+)
+
+string(REGEX MATCH ".*time_macros.*"
+    COTIRE_CCACHE_CONFIG_TIME_MACROS ${CCACHE_CONFIG}
+)
+
+string(REGEX MATCH ".*pch_defines.*"
+    COTIRE_CCACHE_CONFIG_PCH_DEFINES ${CCACHE_CONFIG}
 )
 
 # Regex should be "sloppiness.*time_macros.*pch_defines.*" but due to bug fixed
 # in https://ccache.samba.org/releasenotes.html#_ccache_3_2_5 we have to leave
-# out pch_defines. Ubuntu 16.04 comes with ccache 3.2.4 ...
-string(REGEX MATCH "sloppiness.*time_macros.*"
-    COTIRE_CCACHE_CONFIG ${CCACHE_CONFIG}
-)
-
-if(NOT COTIRE_CCACHE_CONFIG)
-    message(FATAL_ERROR "CCache not configured! You must set sloppiness to pch_defines,time_macros. See https://docs.opengeosys.org/docs/devguide/advanced/using-ccache")
+# out pch_defines if ccache version is older than 3.2.5.
+# Ubuntu 16.04 comes with ccache 3.2.4 ...
+if(NOT COTIRE_CCACHE_CONFIG_TIME_MACROS OR (CCACHE_VERSION_GREATER_EQUAL_3_2_5 AND NOT COTIRE_CCACHE_CONFIG_PCH_DEFINES))
+    message(FATAL_ERROR "CCache configuration does not set sloppiness to pch_defines,time_macros. \
+    Current options are: '${CCACHE_CONFIG}'. \
+    See https://docs.opengeosys.org/docs/devguide/advanced/using-ccache")
 endif()
